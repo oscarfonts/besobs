4bc5a337ddc3497fbe3343deaa100ae0ca9420d0
Marti Pericay <marti.pericay@gmail.com>
2015-11-10 16:40:24 +0100

    a marker and some scripting for the map

--- src/client/observacio.js
+++ src/client/observacio.js
@@ -1,11 +1,36 @@
+//we will organise it better, I promise
 $( document ).ready(function() {
+	
+    //datepicker stuff
     $('.datepicker').datepicker({
     	//format: 'dd/mm/yyyy',
     	language: 'ca-ES' 
     });
     
-    var map = L.map('map').setView([41.43, 2.22], 14);
+    
+    //map stuff
+    var map = L.map('map').setView([41.425, 2.221], 14);
     var Hydda_Full = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
 		attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
+	
+	var marker = L.marker(new L.LatLng(41.425, 2.221), {
+	    draggable: true
+	}).addTo(map);
+	
+	//reflect marker dragging into x and y boxes
+	var xBox = $('#xId');
+	var yBox = $('#yId');
+	marker.on('dragend', function (e) {
+		var latlng = e.target.getLatLng();
+		xBox.val(latlng.lng.toFixed(5));
+		yBox.val(latlng.lat.toFixed(5));
+	});
+	
+	//reflect x and y box changes into marker dragging 
+	xBox.change(moveMarker);
+	yBox.change(moveMarker);
+	function moveMarker(e) {
+		marker.setLatLng(new L.LatLng(yBox.val(), xBox.val()));
+	}
 });
