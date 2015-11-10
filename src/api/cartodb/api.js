448944fe0aff3d32a5b3217dd9b5ddce669a3bd6
Oscar Fonts <oscar.fonts@gmail.com>
2015-11-10 17:16:27 +0100

    POST, PUT and DELETE functionality, not filtered by user. Work on #5

--- src/api/cartodb/api.js
+++ src/api/cartodb/api.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     cartodb = require('cartodb'),
-    secret  = require('./secret.js'),
+    secret = require('./secret.js'),
     Promise = require('promise');
 
 var router = express.Router();
@@ -10,14 +10,32 @@ var router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: false}));
 
-var connect = function(asGeoJSON) {
-    return new Promise(function(fulfill) {
+
+var escape1 = function (string) {
+    return "'" + string.replace("'", "''") + "'";
+};
+
+var escape2 = function (string) {
+    return '"' + string.replace('"', '""') + '"';
+};
+
+var getUrl = function (req) {
+    var url = req.protocol + "://" + req.get('host');
+    url += req.originalUrl;
+    if (url[url.length - 1] != "/") {
+        url += "/";
+    }
+    return url;
+};
+
+var connect = function (asGeoJSON) {
+    return new Promise(function (fulfill) {
         var params = {
             user: secret.USER,
             api_key: secret.API_KEY
         };
 
-        if(asGeoJSON) {
+        if (asGeoJSON) {
             params["api_url"] = "https://" + secret.USER + ".cartodb.com/api/v2/sql?format=GeoJSON";
         }
 
@@ -31,17 +49,17 @@ var connect = function(asGeoJSON) {
     });
 };
 
-var query = function(sql, params, asGeoJSON) {
-    return new Promise(function(fulfill, reject) {
-        var callback = function(error, data) {
-            if(error) {
+var query = function (sql, params, asGeoJSON) {
+    return new Promise(function (fulfill, reject) {
+        var callback = function (error, data) {
+            if (error) {
                 reject(error);
             } else {
                 fulfill(data);
             }
         };
 
-        var query = function(client) {
+        var query = function (client) {
             client.query(sql, params, callback);
         };
 
@@ -49,11 +67,11 @@ var query = function(sql, params, asGeoJSON) {
     })
 };
 
-var success = function(data) {
+var success = function (data) {
     this.send(data);
 };
 
-var error = function(error) {
+var error = function (error) {
     this.status(500).send(error);
 };
 
@@ -62,16 +80,11 @@ router.get('/', function (req, res) {
 
     function parse(data) {
         var tables = [];
-        for(var i in data.rows) {
-            var url = req.protocol + "://" + req.get('host');
-            url += req.originalUrl;
-            if (url[url.length-1] != "/") {
-                url += "/";
-            }
-            url += data.rows[i]["cdb_usertables"];
+        for (var i in data.rows) {
+            var table = data.rows[i]["cdb_usertables"];
             tables.push({
-                name: data.rows[i]["cdb_usertables"],
-                url: url
+                name: table,
+                url: getUrl(req) + table
             });
         }
         res.send({tables: tables});
@@ -81,60 +94,90 @@ router.get('/', function (req, res) {
 });
 
 router.get('/:table', function (req, res) {
+    // TODO Filtrar por req.user
+
     var sql = "SELECT * FROM {table}";
-    var params = {table: req.params.table};
+    var params = {table: escape2(req.params.table)};
 
     query(sql, params, true).then(success.bind(res), error.bind(res));
 });
 
 router.get('/:table/:id', function (req, res) {
+    // TODO Filtrar por req.user
+
     var sql = "SELECT * FROM {table} WHERE cartodb_id={id}";
     var params = {
-        table: req.params.table,
-        id: req.params.id
+        table: escape2(req.params.table),
+        id: escape1(req.params.id)
     };
 
     query(sql, params, true).then(success.bind(res), error.bind(res));
 });
 
 router.post('/:table', function (req, res) {
-    var data = {
-        user: req.user,
-        table: req.params.table,
-        body: "TODO" // TODO recuperar los datos del POST
+    // TODO Insertar req.user
+
+    var table = escape2(req.params.table);
+    var keys = [];
+    var values = [];
+    for (var key in req.body) {
+        keys.push(escape2(key));
+        values.push(escape1(req.body[key]));
+    }
+
+    var sql = "INSERT INTO " + table + " (" + keys.join(", ") + ") VALUES (" + values.join(", ") + ") RETURNING cartodb_id";
+
+    var parse = function (data) {
+        var response = data.rows[0];
+        response.url = getUrl(req) + data.rows[0]["cartodb_id"];
+        res.send(response);
     };
-    console.log(data);
-    // TODO insertar el registro
-    // TODO devolver el nuevo registro, junto con el ID (!)
-    res.send(data);
+
+    query(sql, null, false).then(parse, error.bind(res));
 });
 
 router.put('/:table/:id', function (req, res) {
-    var data = {
-        user: req.user,
-        table: req.params.table,
-        id: req.params.id,
-        body: "TODO" // TODO recuperar los datos del PUT
+    // TODO Filtrar por -e insertar- req.user
+
+    var table = escape2(req.params.table);
+    var id = escape1(req.params.id);
+    var keys = [];
+    var values = [];
+    req.body.cartodb_id = req.params.id;
+    for (var key in req.body) {
+        keys.push(escape2(key));
+        values.push(escape1(req.body[key]));
+    }
+
+    var sql = "UPDATE " + table + " SET (" + keys.join(", ") + ") = (" + values.join(", ") + ") WHERE cartodb_id = " + id + " RETURNING cartodb_id";
+
+    var parse = function (data) {
+        var response = data.rows[0];
+        response.url = getUrl(req);
+        res.send(response);
     };
-    console.log(data);
-    // TODO comprobar que el registro realmente existe
-    // TODO modificar el registro
-    // TODO devolver el nuevo registro
-    res.send(data);
+
+    query(sql, null, false).then(parse, error.bind(res));
 });
 
 router.delete('/:table/:id', function (req, res) {
-    // TODO comprobar que no existe el registro
-    var data = {
-        user: req.user,
-        table: req.params.table,
-        id: req.params.id
+    // TODO Filtrar por req.user
+
+    var table = escape2(req.params.table);
+    var id = escape1(req.params.id);
+
+    var sql = "DELETE FROM " + table + " WHERE cartodb_id = " + id + " RETURNING cartodb_id";
+
+    var parse = function (data) {
+        if (data.rows.length) {
+            var response = data.rows[0];
+            res.send(response);
+        } else {
+            res.send({message: "No rows affected"});
+        }
     };
-    console.log(data);
-    // TODO comprobar que el registro realmente existe
-    // TODO borrar el registro
-    // TODO devolver el ID del registro recién borrado
-    res.send(data);
+
+    query(sql, null, false).then(parse, error.bind(res));
 });
 
 module.exports = router;
