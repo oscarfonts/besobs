f15eedfd3cc8f97846ff047bf2953bdb435ff8fa
Marti Pericay <marti.pericay@gmail.com>
2015-11-10 15:47:42 +0100

    add simple map

--- src/client/observacio.js
+++ src/client/observacio.js
@@ -3,4 +3,9 @@ $( document ).ready(function() {
     	//format: 'dd/mm/yyyy',
     	language: 'ca-ES' 
     });
+    
+    var map = L.map('map').setView([41.43, 2.22], 14);
+    var Hydda_Full = L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
+		attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
 });
